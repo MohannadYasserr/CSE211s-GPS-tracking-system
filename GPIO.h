b51fe5c2a042c0f1337a07b6ebb95d0cf6acdf99
Mohameddwaelll <168580066+Mohameddwaelll@users.noreply.github.com>
2024-05-02 12:46:54 +0300

    Add files via upload

---
+++ GPIO.h
@@ -0,0 +1,10 @@
+#include "BITUTILITIES.h"
+#include "tm4c123gh6pm.h"
+
+
+void led_off(unsigned char led);
+void led_on(unsigned char led);
+void GPIO_initPORTF(void);
+void SW1_Init(void);
+void SW2_Init(void);
+
